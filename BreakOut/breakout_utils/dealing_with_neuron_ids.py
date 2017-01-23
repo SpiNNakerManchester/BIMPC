edd18d166cd3071fdf92dbe86e48a31943a94dee
Petrut Antoniu Bogdan <petrut.bogdan@student.manchester.ac.uk>
2017-01-23 19:35:01 +0000

    handy utilities to create pools of neurons. constants separated into their own file. ball following advanced by creating left and right receptive fields etc

--- BreakOut/breakout_utils/dealing_with_neuron_ids.py
+++ BreakOut/breakout_utils/dealing_with_neuron_ids.py
@@ -1,26 +1,8 @@
 import numpy as np
-
-# Magic constants -- NEED TO MANUALLY KEEP UPDATED TO MATCH C MODULE
-GAME_WIDTH = 160
-GAME_HEIGHT = 128
-SPECIAL_EVENT_SCORE_UP = 0
-SPECIAL_EVENT_SCORE_DOWN = 1
-SPECIAL_EVENT_MAX = 2
-
-COLOUR_ON = 1
-COLOUR_OFF = 0
-
-KEY_LEFT = 0
-KEY_RIGHT = 1
-
-on_neuron_ids = None
-off_neuron_ids = None
+from constants import *
 
 
 def get_on_neuron_ids(width=GAME_WIDTH, height=GAME_HEIGHT):
-    global on_neuron_ids
-    if on_neuron_ids:
-        return on_neuron_ids
     on_neuron_ids = np.zeros((height, width), dtype=np.int32)
     colour = COLOUR_ON
     for i in range(width):
@@ -30,9 +12,6 @@ def get_on_neuron_ids(width=GAME_WIDTH, height=GAME_HEIGHT):
 
 
 def get_off_neuron_ids(width=GAME_WIDTH, height=GAME_HEIGHT):
-    global off_neuron_ids
-    if off_neuron_ids:
-        return off_neuron_ids
     off_neuron_ids = np.zeros((height, width), dtype=np.int32)
     colour = COLOUR_OFF
     for i in range(width):
