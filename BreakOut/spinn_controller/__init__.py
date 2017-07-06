d86229006b44f05613b0639b324ca9f4163e776b
simond68 <simon.davidson@manchester.ac.uk>
2017-07-06 15:13:39 +0100

    classical SARSA controller on SpiNNaker

---
+++ BreakOut/spinn_controller/__init__.py
@@ -0,0 +1,9 @@
+from spynnaker.pyNN.spinnaker import executable_finder
+from model_binaries import __file__ as binaries_path
+from python_models.rl_controller import RLController
+from visualiser.visualiser import Visualiser
+from visualiser.visualiser_subsamp import Visualiser_subsamp
+import os
+
+# This adds the model binaries path to the paths searched by sPyNNaker
+executable_finder.add_path(os.path.dirname(binaries_path))
