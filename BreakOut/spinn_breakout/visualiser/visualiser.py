ee1b2e1387f68312d9a3821c1ba650438cd5e28f
such-a-git <adamperrett3142@gmail.com>
2018-09-05 15:43:02 +0100

    bricks now transferred from brick branch to GA, also included visualiser

--- BreakOut/spinn_breakout/visualiser/visualiser.py
+++ BreakOut/spinn_breakout/visualiser/visualiser.py
@@ -1,3 +1,5 @@
+import multiprocessing
+
 import enum
 import numpy as np
 import socket
@@ -10,18 +12,26 @@ import datetime
 # from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import cv2
 import io
+import os
 
 BRIGHT_GREEN = (0.0, 0.9, 0.0)
+BRIGHT_RED = (0.9, 0.0, 0.0)
+BRIGHT_BLUE = (0, 0.0, 0.9)
+BRIGHT_PURPLE = (0.9, 0.0, 0.9)
+BRIGHT_ORANGE = (0.9, 0.4, 0.0)
 VIDEO_GREEN = np.array([0, 230, 0])
 VIDEO_RED = np.array([230, 0, 0])
+
+
 # ----------------------------------------------------------------------------
 # InputState
 # ----------------------------------------------------------------------------
 # Input states
 class InputState(enum.IntEnum):
     idle = -1
-    left = 1
-    right = 0
+    left = 0
+    right = 1
+
 
 # ----------------------------------------------------------------------------
 # SpecialEvent
@@ -32,12 +42,15 @@ class SpecialEvent(enum.IntEnum):
     score_down = 1
     max = 2
 
+
 # ----------------------------------------------------------------------------
 # Visualiser
 # ----------------------------------------------------------------------------
 class Visualiser(object):
     # How many bits are used to represent colour
-    colour_bits = 1
+    colour_bits = 2
+    BRICK_WIDTH = 10
+    BRICK_HEIGHT = 6
 
     def __init__(self, udp_port, key_input_connection=None, scale=4,
                  x_res=160, y_res=128, x_bits=8, y_bits=8, fps=60):
@@ -53,17 +66,25 @@ class Visualiser(object):
         # Build masks
         self.x_mask = (1 << x_bits) - 1
         self.x_shift = self.colour_bits + y_bits
+        assert self.x_shift == 10, self.x_shift
 
         self.y_mask = (1 << y_bits) - 1
         self.y_shift = self.colour_bits
 
-        self.colour_mask = (1 << self.colour_bits) - 1
+        assert self.y_shift == 2, self.y_shift
+
+        self.colour_mask = 1
+        self.bricked_mask = 2
 
         self.value_mask = (1 << (x_bits + y_bits + self.colour_bits)) - 1
 
-        self.y_res=y_res
-        self.x_res=x_res
-        self.bat_width=16
+        assert x_bits + y_bits + self.colour_bits == 18, x_bits + y_bits + self.colour_bits
+
+        assert self.value_mask == 0x3FFFF, self.value_mask
+
+        self.y_res = y_res
+        self.x_res = x_res
+        self.bat_width = 16
         self.fps = fps
         self.scale = scale
 
@@ -73,15 +94,15 @@ class Visualiser(object):
         self.socket.setblocking(False)
 
         # Make awesome CRT palette
-        cmap = col.ListedColormap(["black", BRIGHT_GREEN])
+        cmap = col.ListedColormap(["black", BRIGHT_GREEN, BRIGHT_RED, BRIGHT_PURPLE, BRIGHT_BLUE, BRIGHT_ORANGE])
 
         # Create image plot to display game screen
         self.fig = plt.figure("BreakOut", figsize=(8, 6))
-        self.axis = plt.subplot(1,1,1)
+        self.axis = plt.subplot(1, 1, 1)
         # self.ion = plt.ion()
         self.image_data = np.zeros((y_res, x_res))
         self.image = self.axis.imshow(self.image_data, interpolation="nearest",
-                                      cmap=cmap, vmin=0.0, vmax=1.0)
+                                      cmap=cmap, vmin=0.0, vmax=5.0)
 
         # Draw score using textbox
         self.score_text = self.axis.text(0.5, 1.0, "0", color=BRIGHT_GREEN,
@@ -91,32 +112,38 @@ class Visualiser(object):
         # Hook key listeners
         self.fig.canvas.mpl_connect("key_press_event", self._on_key_press)
         self.fig.canvas.mpl_connect("key_release_event", self._on_key_release)
+        self.fig.canvas.mpl_connect('close_event', self.handle_close)
         # Hide grid
         self.axis.grid(False)
         self.axis.set_xticklabels([])
         self.axis.set_yticklabels([])
         self.axis.axes.get_xaxis().set_visible(False)
 
-        #fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        # self.video_data = np.zeros((self.y_res, self.x_res, 3), dtype='uint8')
-        # # self.video_data = cv2.imread("temp_frame.png")
-        # self.video_shape = (self.x_res*self.scale, self.y_res*self.scale)
-        # self.dsize = (self.y_res*self.scale, self.x_res*self.scale)
-        #
-        # self.video_writer = cv2.VideoWriter(
-        #     "breakout_output_%s.m4v" %
-        #     datetime.datetime.now().
-        #     strftime("%Y-%m-%d___%H-%M-%S"),
-        #     fourcc, self.fps,
-        #     self.video_shape,
-        #     isColor=True)
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        self.video_data = np.zeros((self.y_res, self.x_res, 3), dtype='uint8')
+        # self.video_data = cv2.imread("temp_frame.png")
+        self.video_shape = (self.x_res * self.scale, self.y_res * self.scale)
+        self.dsize = (self.y_res * self.scale, self.x_res * self.scale)
+
+        filename = os.path.join(os.getcwd(), "breakout_output_%s.m4v" %
+                    datetime.datetime.now().strftime("%Y-%m-%d___%H-%M-%S"))
+        # print filename
+        self.video_writer = cv2.VideoWriter(
+            filename,
+            fourcc, self.fps,
+            self.video_shape,
+            isColor=True)
+        self.video_writer.open(filename,
+            fourcc, self.fps,
+            self.video_shape,
+            isColor=True)
 
     # ------------------------------------------------------------------------
     # Public methods
     # ------------------------------------------------------------------------
     def show(self):
         # Play animation
-        interval = (1000./self.fps)
+        interval = (1000. / self.fps)
         self.animation = animation.FuncAnimation(self.fig, self._update,
                                                  interval=interval,
                                                  blit=False)
@@ -126,6 +153,8 @@ class Visualiser(object):
         except:
             pass
 
+    def handle_close(self, evt):
+        self.video_writer.release()
 
     # ------------------------------------------------------------------------
     # Private methods
@@ -158,12 +187,13 @@ class Visualiser(object):
                 # Create mask to select vision (rather than special event) packets
 
                 # Extract coordinates
-                vision_payload = payload_value[vision_event_mask] - SpecialEvent.max
+                vision_payload = payload_value[
+                                     vision_event_mask] - SpecialEvent.max
                 x = (vision_payload >> self.x_shift) & self.x_mask
                 y = (vision_payload >> self.y_shift) & self.y_mask
 
                 c = (vision_payload & self.colour_mask)
-
+                b = (vision_payload & self.bricked_mask) >> 1
 
                 '''if y.any() == self.y_res-1:
                     if c[np.where(y==self.y_res-1)].any()==1:
@@ -175,21 +205,30 @@ class Visualiser(object):
                             np.hstack((x,x_pos+i))'''
                 # Set valid pixels
                 try:
-                    # self.image_data[:] = 0
-                    self.image_data[y, x] = c
+                    for x1, y1, c1, b1 in zip(x, y, c, b):
+                        # self.image_data[:] = 0
+                        if b1 == 0:
+                            self.image_data[y1, x1] = c1
+                        elif b1 == 1:
+                            self.image_data[y1:(y1 + self.BRICK_HEIGHT),
+                            x1:(x1 + self.BRICK_WIDTH)] = c1 * np.random.randint(2, 6)
+
                     # if c>0:
                     # self.video_data[:] = 0
-                    # self.video_data[y, x, 1] = np.uint8(c*230)
+                    self.video_data[y, x, 1] = np.uint8(c * 230)
                     # else:
                     #     self.video_data[y, x, :] = VIDEO_RED
                 except IndexError as e:
                     print("Packet contains invalid pixels:",
-                          vision_payload, "X:", x, "  Y:",y, " c:",c)
+                          vision_payload, "X:", x, "  Y:", y, " c:", c, " b:",
+                          b)
                     # self.image_data[:-1, :] = 0
 
                 # Create masks to select score events and count them
-                num_score_up_events = np.sum(payload_value == SpecialEvent.score_up)
-                num_score_down_events = np.sum(payload_value == SpecialEvent.score_down)
+                num_score_up_events = np.sum(
+                    payload_value == SpecialEvent.score_up)
+                num_score_down_events = np.sum(
+                    payload_value == SpecialEvent.score_down)
 
                 # If any score events occurred
                 if num_score_up_events > 0 or num_score_down_events > 0:
@@ -200,15 +239,15 @@ class Visualiser(object):
                     # Update displayed score count
                     self.score_text.set_text("%u" % self.score)
 
-                # if self.score > 0:
-                #     # print("pos score %d"%self.score)
-                #     self.video_data[0:1, :, :] = [100, 255, 255]
-                # elif self.score < 0:
-                #     # print("neg score %d"%self.score)
-                #     self.video_data[0:1, :, :] = [255, 100, 255]
-                # else:
-                #     # print("score 0")
-                #     self.video_data[0:1, :, :] = [200, 200, 200]
+                if self.score > 0:
+                    # print("pos score %d"%self.score)
+                    self.video_data[0:1, :, :] = [100, 255, 255]
+                elif self.score < 0:
+                    # print("neg score %d"%self.score)
+                    self.video_data[0:1, :, :] = [255, 100, 255]
+                else:
+                    # print("score 0")
+                    self.video_data[0:1, :, :] = [200, 200, 200]
 
         # Set image data
         try:
@@ -217,19 +256,19 @@ class Visualiser(object):
             pass
 
         # try:
-        #     if message_received :
-        #     #     if not self.first_update:
-        #     #         buf = io.BytesIO()
-        #     #         plt.savefig(buf, format='png', dpi=100)
-        #     #         buf.seek(0)
-        #     #         self.video_data[:] = cv2.imdecode(
-        #     #             np.fromstring(buf.read(), np.uint8),
-        #     #             cv2.IMREAD_COLOR)
-        #     #     self.video_writer.write(self.video_data)
-        #
-        #         self.video_writer.write(
-        #             cv2.resize(self.video_data, self.video_shape,
-        #                        interpolation=cv2.INTER_NEAREST))
+        if message_received:
+            #     if not self.first_update:
+            #         buf = io.BytesIO()
+            #         plt.savefig(buf, format='png', dpi=100)
+            #         buf.seek(0)
+            #         self.video_data[:] = cv2.imdecode(
+            #             np.fromstring(buf.read(), np.uint8),
+            #             cv2.IMREAD_COLOR)
+            #     self.video_writer.write(self.video_data)
+
+            self.video_writer.write(
+                cv2.resize(self.video_data, self.video_shape,
+                           interpolation=cv2.INTER_NEAREST))
         # except:
         #     pass
 
@@ -259,4 +298,4 @@ if __name__ == "__main__":
     UDP_PORT = 17893
     vis = Visualiser(UDP_PORT)
     vis.show()
-    vis.video_writer.release()
+
